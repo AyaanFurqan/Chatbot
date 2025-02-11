e887dead8ce1115f6182bd51e8bfe52e95c131ca
Ayaan <ayaanfurqan008@gmail.com>
2025-02-11 09:31:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+function abc() {
+    let user = document.getElementById("user-message");
+    let chatbot = document.getElementById("chat-message")
+
+    chatbot.innerHTML += `<div class="chat-message user">
+    <p> You : ${user.value}</p>
+</div>`
+
+    user.value = ""
+
+    let usermessage = user.value.toLowerCase()
+    if (usermessage == "i want to know about courses") {
+        chatbot.innerHTML += `<div class = "chat-message">
+ <p> Bot:Yes there is new batch starting from wednesday. you can enroll now.
+`
+    }
+}
